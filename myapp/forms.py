be69a836ffdaa6abf26111bc34e18dcd054abce1
Yash Dwivedi <contactyash.555@gmail.com>
2021-10-28 23:30:27 +0530

    fourth

--- myapp/forms.py
+++ myapp/forms.py
@@ -4,7 +4,6 @@ class MyForm(forms.Form):
     firstname = forms.CharField(label="First Name",max_length=30)
     lastname = forms.CharField(label="Last Name",max_length=30)
     email = forms.EmailField(label="Email")
-    comments = forms.CharField(widget=forms.Textarea(attrs={'rows':5,'cols':50,'style':'resize:none;'}),required=False)
-
     CHOICES = ((5,'Best'),(4,'Good'),(3,'Average'),(2,'Bad'),(1,'Worst'))
-    rating = forms.ChoiceField(choices=CHOICES,widget=forms.RadioSelect)
+    rating = forms.ChoiceField(label="Rate My Site!",choices=CHOICES,widget=forms.RadioSelect)
+    comments = forms.CharField(label="Suggestions are welcome!",widget=forms.Textarea,required=False)
